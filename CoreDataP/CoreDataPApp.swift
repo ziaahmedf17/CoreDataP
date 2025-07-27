da2584e4a105374cc2495152eee00c22f5f342c4
Zia Ahmed <ziaahmedf17@gmail.com>
2025-07-27 06:37:42 +0500

    Initial Commit

---
+++ CoreDataP/CoreDataPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataPApp.swift
+//  CoreDataP
+//
+//  Created by Zi on 27/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoreDataPTests/CoreDataPTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataPTests.swift
+//  CoreDataPTests
+//
+//  Created by Zi on 27/07/2025.
+//
+
+import Testing
+@testable import CoreDataP
+
+struct CoreDataPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
